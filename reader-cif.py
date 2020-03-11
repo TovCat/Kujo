0ee11efb1799566680a0791d32f3c44e1eec1099
Igor Koskin <osingran@yandex.ru>
2020-03-11 11:08:18 +0100

    PyCharm configuration

--- reader-cif.py
+++ reader-cif.py
@@ -519,7 +519,8 @@ class Cluster:
                     if connected:
                         break
                     for i4 in range(self.pre_molecules[0].num_atoms):
-                        if bonds[i1 * self.pre_molecules[0].num_atoms + i3, i2 * self.pre_molecules[0].num_atoms + i4] == 1:
+                        if bonds[
+                            i1 * self.pre_molecules[0].num_atoms + i3, i2 * self.pre_molecules[0].num_atoms + i4] == 1:
                             au_bonds[i1, i2] = 1
                             au_bonds[i2, i1] = 1
                             connected = True
@@ -560,7 +561,6 @@ class Cluster:
             self.mass_centers[i, 2] = v[2, 0]
         print("   Done: %s" % (time.time() - simp_time))
 
-
     def to_cartesian(self):
         for i1 in range(len(self.pre_molecules)):
             for i in range(23):
@@ -590,7 +590,7 @@ mu = np.zeros((1, 3))
 mu[0, 0] = 5.09777
 mu[0, 1] = 0.07312
 mu[0, 2] = 0.04931
-A = 219500 # Hartree to cm-1
+A = 219500  # Hartree to cm-1
 bohr3 = 0.529177210  # bohr in Å
 H = np.zeros((len(cl.molecules), len(cl.molecules)))
 for n1 in range(len(cl.molecules)):
@@ -606,10 +606,10 @@ for n1 in range(len(cl.molecules)):
         dv1 = v1_o - v2_o
         r1 = np.linalg.norm(dv1)
         r1 = r1 / bohr3
-        r3 = r1*r1*r1
-        r5 = r3*r1*r1
-        J = np.inner(mu, mu)/r3 + (np.inner(mu, dv1)*np.inner(dv1, mu))/r5
-        H[n1, m1] = J*A
+        r3 = r1 * r1 * r1
+        r5 = r3 * r1 * r1
+        J = np.inner(mu, mu) / r3 + (np.inner(mu, dv1) * np.inner(dv1, mu)) / r5
+        H[n1, m1] = J * A
         H[m1, n1] = H[n1, m1]
 print("   Done: %s" % (time.time() - h_time))
 mu_a = np.zeros((len(cl.molecules), 3))
@@ -638,12 +638,12 @@ for n1 in range(len(cl.molecules)):
 
 # Convolute spectrum
 # First create normalized Gaussian centered in the middle
-Cx=Ex-(Emax+Emin)/2 # Make new axis with value zero in the middle of array
-Cy=np.exp(-Cx**2/2/sigma**2)/np.sqrt(2*np.pi*sigma**2)
+Cx = Ex - (Emax + Emin) / 2  # Make new axis with value zero in the middle of array
+Cy = np.exp(-Cx ** 2 / 2 / sigma ** 2) / np.sqrt(2 * np.pi * sigma ** 2)
 # Do the actual convolusion
-Ny=np.convolve(Ey,Cy,mode='same')
+Ny = np.convolve(Ey, Cy, mode='same')
 # Plot everything in one final plot
-plt.plot(Ex,Ey/np.max(Ey))
-#plt.plot(Ex,Cy/np.max(Cy))
-plt.plot(Ex,Ny/np.max(Ny))
+plt.plot(Ex, Ey / np.max(Ey))
+# plt.plot(Ex,Cy/np.max(Cy))
+plt.plot(Ex, Ny / np.max(Ny))
 plt.show()
