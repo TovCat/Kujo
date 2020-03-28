877eed169e554cd757460d55f63eac0cf60b7cf3
Igor Koskin <osingran@yandex.ru>
2020-03-28 19:40:53 +0100

    Fixed reader_cube.py module

--- reader_cube.py
+++ reader_cube.py
@@ -16,7 +16,7 @@ class Cube:
         words = contents[2].split()
         self.num_atoms = int(words[0])
         self.origin = np.array([float(words[1]), float(words[2]), float(words[3])])
-        self.steps = np.zeros((3, 1))
+        self.steps = np.zeros((3, 1), dtype=int)
         self.volume = np.zeros((3, 3))
         for i in range(3):
             words = contents[i + 3].split()
@@ -27,16 +27,16 @@ class Cube:
         self.molecule = rc.Molecule(self.num_atoms)
         for i in range(6, 6 + self.num_atoms):
             words = contents[i].split()
-            self.molecule.atom_label[i - 6] = dic.nrelements[int(words[0])]
-            self.molecule.atom_coord[i:i + 1] = np.array([float(words[2]), float(words[3], float(words[4]))])
-        n = 0
+            self.molecule.atom_label.append(dic.nrelements[int(words[0])])
+            self.molecule.atom_coord[i:i + 1] = np.array([float(words[2]), float(words[3]), float(words[4])])
         self.voxels = np.zeros((self.steps[0, 0], self.steps[1, 0], self.steps[2, 0]))
-        for i in range(6 + self.num_atoms, len(contents)):
-            words = contents[i].split()
-            for i1 in range(len(words)):
-                z = n % self.steps[2, 0]
-                y = (n - z) % self.steps[1, 0]
-                x = n - y * self.steps[1, 0] - z
-                self.voxels[x, y, z] = float(words[i1])
-                n = n + 1
-
+        sep_contents = []
+        for i1 in range(6 + self.num_atoms, len(contents)):
+            words = contents[i1].split()
+            for i2 in range(len(words)):
+                sep_contents.append(float(words[i2]))
+        for x in range(self.steps[0, 0]):
+            for y in range(self.steps[1, 0]):
+                for z in range(self.steps[2, 0]):
+                    n = z + y * self.steps[1, 0] + x * self.steps[0, 0]
+                    self.voxels[x, y, z] = sep_contents[n]
