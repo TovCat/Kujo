a71db414d9bdd9d52165a0cccb960757b37a43da
Igor Koskin <osingran@yandex.ru>
2020-04-03 11:32:58 +0200

    Print cluster xyz

--- kujo_io.py
+++ kujo_io.py
@@ -1,4 +1,5 @@
 from os import getcwd
+import reader_cif
 
 
 def output_error(text: str, error_code: int):
@@ -27,5 +28,20 @@ def read_input(path: str):
         if len(words) == 1:
             output_error("Syntax error at the input file: " + x, -1)
         instructions.append(words[0])
-        options = words[1].split(",")
-    return  instructions, options
+        options.append(words[1].split(","))
+    return instructions, options
+
+
+def print_cluster_xyz(cl: reader_cif.Cluster, path: str):
+    full_path = getcwd()
+    file = open(full_path + "/cluster.xyz", "w")
+    for n in range(len(cl.molecules)):
+        for m in range(cl.molecules[n].num_atoms):
+            l = cl.molecules[n].atom_label[m]
+            x = repr(round(cl.molecules[n].atom_coord[m, 0], 6))
+            y = repr(round(cl.molecules[n].atom_coord[m, 1], 6))
+            z = repr(round(cl.molecules[n].atom_coord[m, 2], 6))
+            file.write(l + x + y + z)
+    file.close()
+
+
