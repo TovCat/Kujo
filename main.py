f3ddcf50a3d88c43112a51c51a10dc05f2e98647
Igor Koskin <osingran@yandex.ru>
2020-04-06 19:45:41 +0200

    Integrate translated cube instruction for dispatcher

--- main.py
+++ main.py
@@ -18,8 +18,27 @@ def integrate_translated_cube(vars: list):
             is_bohr = bool(vars[3])
     except ValueError:
         kujo_io.output_error("Value type error at options!", -1)
+        pass
+    low_limits = []
+    up_limits = []
+    low_limits.append(np.array([0, 0, 0]))
+    step = np.array([cube.steps[0, 0] // threads_number, cube.steps[1, 0], cube.steps[2, 0]])
+    for _ in range(threads_number - 1):
+        up_limits.append(low_limits[-1] + step)
+        low_limits.append(up_limits[-1])
+    up_limits.append(np.array([cube.steps[0, 0], cube.steps[1, 0], cube.steps[2, 0]]))
     if is_bohr is None:
-
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = [executor.submit(cube.integrate, low_limits[i], up_limits[i], translate)
+                       for i in range(threads_number)]
+    else:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = [executor.submit(cube.integrate, low_limits[i], up_limits[i], translate, is_bohr)
+                       for i in range(threads_number)]
+    r = 0
+    for x in results:
+        r = r + x
+    return r
 
 
 
