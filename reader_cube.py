7c93326713bfa3bbea9a88c2bd9c946eaf54d173
Igor Koskin <osingran@yandex.ru>
2020-04-06 15:47:01 +0200

    Parallel scheme for cube integrate method

--- reader_cube.py
+++ reader_cube.py
@@ -3,10 +3,6 @@ import reader_cif as rc
 import dictionaries as dic
 
 
-A = 219500  # Hartree to cm-1
-bohr3 = 0.529177210  # bohr in Å
-
-
 class Cube:
 
     def __init__(self, path=""):
@@ -44,45 +40,23 @@ class Cube:
                 for z in range(self.steps[2, 0]):
                     n = z + y * self.steps[1, 0] + x * self.steps[0, 0]
                     self.voxels[x, y, z] = sep_contents[n]
+        self.dv = self.volume[0, 0] * self.volume[1, 0] * self.volume[2, 0]
 
-
-def integrate(cub: Cube, type1: bool, translate: np.array):
-    mc = cub.molecule.mass_center()
-    dv = cub.volume[0, 0] * cub.volume[1, 0] * cub.volume[2, 0]
-    J = 0
-    if not type1:
-        for x1 in range(cub.steps[0, 0]):
-            for y1 in range(cub.steps[1, 0]):
-                for z1 in range(cub.steps[2, 0]):
-                    print(np.array([x1, y1, z1]))
-                    for x2 in range(cub.steps[0, 0]):
-                        for y2 in range(cub.steps[1, 0]):
-                            for z2 in range(cub.steps[2, 0]):
-                                v1 = np.array([x1 * cub.volume[0, 0], y1 * cub.volume[1, 0], z1 * cub.volume[2, 0]]) + \
-                                     cub.origin
-                                v2 = np.array([x2 * cub.volume[0, 0], y2 * cub.volume[1, 0], z2 * cub.volume[2, 0]]) + \
-                                     cub.origin + translate
+    def integrate(self, low_limit, up_limit, translate: np.array, is_bohr = False):
+        J = 0
+        if not is_bohr:
+            translate = translate / dic.bohr3
+        for x1 in range(low_limit[0, 0], up_limit[0, 0]):
+            for y1 in range(low_limit[1, 0], up_limit[1, 0]):
+                for z1 in range(low_limit[2, 0], up_limit[2, 0]):
+                    for x2 in range(self.steps[0, 0]):
+                        for y2 in range(self.steps[1, 0]):
+                            for z2 in range(self.steps[2, 0]):
+                                v1 = np.array([x1 * self.volume[0, 0], y1 * self.volume[1, 0], z1 * self.volume[2, 0]]) + \
+                                     self.origin
+                                v2 = np.array([x2 * self.volume[0, 0], y2 * self.volume[1, 0], z2 * self.volume[2, 0]]) + \
+                                     self.origin + translate
                                 r = np.linalg.norm(v1 - v2)
-                                J = J + cub.voxels[x1, y1, z1] * cub.voxels[x2, y2, z2] / r
-    else:
-        origin2 = cub.origin
-        origin2[0] = origin2[0] - mc[0, 0]
-        origin2[2] = origin2[2] - mc[0, 2]
-        origin2[0] = -1 * origin2[0]
-        origin2[2] = -1 * origin2[2]
-        origin2[0] = origin2[0] + mc[0, 0]
-        origin2[2] = origin2[2] + mc[0, 2]
-        for x1 in range(cub.steps[0, 0]):
-            for y1 in range(cub.steps[1, 0]):
-                for z1 in range(cub.steps[2, 0]):
-                    print(np.array([x1, y1, z1]))
-                    for x2 in range(cub.steps[0, 0]):
-                        for y2 in range(cub.steps[1, 0]):
-                            for z2 in range(cub.steps[2, 0]):
-                                v1 = np.array([x1 * cub.volume[0, 0], y1 * cub.volume[1, 0],
-                                               z1 * cub.volume[2, 0]]) + cub.origin
-                                v2 = np.array([-1 * x2 * cub.volume[0, 0], y2 * cub.volume[1, 0],
-                                               -1 * z2 * cub.volume[2, 0]]) + origin2 + translate
-                                r = np.linalg.norm(v1 - v2)
-                                J = J + cub.voxels[x1, y1, z1] * cub.voxels[x2, y2, z2] / r
-    return J
+                                J = J + (self.voxels[x1, y1, z1] * self.voxels[x2, y2, z2] / r) * self.dv
+        J = J / dic.A
+        return J
