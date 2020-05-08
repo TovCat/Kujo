bb7f7fab65db900220c24fbdfe4a01bc72bb3440
Igor Koskin <osingran@yandex.ru>
2020-05-08 04:30:43 +0200

    Charges reader

---
+++ reader/charges.py
@@ -0,0 +1,20 @@
+import reader.cif
+import numpy as np
+
+
+class Charges:
+
+    def __init__(self, file):
+        file = open(file, "r")
+        contents = file.readlines()
+        file.close()
+        self.mol = reader.cif.Molecule(len(contents))
+        self.q = np.zeros((len(contents)))
+        for x in contents:
+            words = x.split()
+            self.mol.atom_label.append(words[0])
+            index = contents.index(x)
+            self.mol.atom_coord[index, 0] = float(words[1])
+            self.mol.atom_coord[index, 1] = float(words[2])
+            self.mol.atom_coord[index, 2] = float(words[3])
+            self.q[index] = float(words[4])
